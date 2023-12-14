a19de8d38bbd3aacf2dce3557926383dcd342e74
jasonviipers <jason@chatr.fr>
2023-12-14 11:01:00 +0100

    add more config for testing

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    roots: ['./src'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.jsx?$': 'babel-jest'
-    }
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  roots: ['./src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  modulePathIgnorePatterns: ['/node_modules/', '/build/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
