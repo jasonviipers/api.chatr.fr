4ec330beeb3d53abe93585737cf37e46858b8412
jasonviipers <jason@chatr.fr>
2023-12-14 11:32:02 +0100

    async fn should always return await

--- src/services/hash.services.ts
+++ src/services/hash.services.ts
@@ -5,13 +5,13 @@ export class HashServices {
     protected plainText: string;
 
     constructor(plainText: string) {
-        const salt = bcrypte.genSaltSync(11);
+        const salt = bcrypte.genSaltSync(12);
         this.plainText = plainText;
         this.salt = parseInt(salt);
     }
 
     public async hash(): Promise<string> {
-        return bcrypte.hashSync(this.plainText, this.salt);
+        return await bcrypte.hashSync(this.plainText, this.salt);
     }
 
     public compare(hash: string): boolean {
